0f2586973da53f5d56c560633604e85c660b437d
1219664875 <jesmanijismi@gmail.com>
2024-06-12 18:23:38 +0800

    Working on fourth transition.

--- src/router/index.js
+++ src/router/index.js
@@ -2,28 +2,34 @@ import { createRouter, createWebHistory } from "vue-router";
 import TimePage from "../views/TimePage.vue";
 import LocationPage from "../views/LocationPage.vue";
 import RsvpPage from "../views/RsvpPage.vue";
+import CountingPage from "../views/CountingPage.vue";
 
 const routes = [
-  {
-    path: "/time",
-    name: "Time",
-    component: TimePage,
-  },
-  {
-    path: "/location",
-    name: "Location",
-    component: LocationPage,
-  },
-  {
-    path: "/rsvp",
-    name: "Rsvp",
-    component: RsvpPage,
-  },
+    {
+        path: "/time",
+        name: "Time",
+        component: TimePage,
+    },
+    {
+        path: "/location",
+        name: "Location",
+        component: LocationPage,
+    },
+    {
+        path: "/rsvp",
+        name: "Rsvp",
+        component: RsvpPage,
+    },
+    {
+        path: "/counting",
+        name: "Counting",
+        component: CountingPage,
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
---
+++ src/views/CountingPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="counting-page">
+        <h1>Counting Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    // No data or methods needed for testing
+};
+</script>
+
+<style scoped>
+.counting-page {
+    text-align: center;
+    padding: 20px;
+}
+
+h1 {
+    font-size: 24px;
+}
+</style>
